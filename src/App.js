f9bdffdd1546de6061344e5b844b9cf86c16637e
bahareh <baharehviani@gmail.com>
2025-08-17 11:44:14 +0330

    simple search for words

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Bahareh Viani</small>
+        </footer>
+      </div>
     </div>
   );
 }
